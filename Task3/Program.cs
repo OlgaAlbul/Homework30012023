1e1b8eed7ada8d21de45a20cdaab94bb5a83f370
AlbulOlya <albul.olya@gmail.com>
2023-01-31 22:52:11 +0500

    Task 3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер четверти");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Диапазоны для возможных координат X>0, Y>0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Диапазоны для возможных координат X<0, Y>0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Диапазоны для возможных координат X<0, Y<0");
+}
+else
+{
+    Console.WriteLine("Диапазоны для возможных координат X>0, Y<0");
+}
