d310b33d655ca3914396be877b312a00374bb595
AlbulOlya <albul.olya@gmail.com>
2023-01-31 22:25:18 +0500

    Task 1 solved

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int number = int.Parse(Console.ReadLine());
+if (number < 6)
+{
+    Console.WriteLine("Не является выходным");
+}
+else 
+{
+    Console.WriteLine("Является выходным");
+}
